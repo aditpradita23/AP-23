6a926f922a6441309b2061342dfe154c1367f258
aditpradita23 <aditpradita154@gmail.com>
2026-09-20 18:05:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if uploaded_file is not None:
                 try:
                     genai.configure(api_key=api_key)
                     # Menggunakan model standar gemini-pro-vision atau gemini-flash
-                    model = genai.GenerativeModel('gemini-1.5-flash')
+                    model = genai.GenerativeModel('gemini-1.5-pro')
 
                     
                     prompt = f"""
